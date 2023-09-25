852d216cb38f53e8d4f44836774811fc7440418b
SergioSanFo <superyo1736@gmail.com>
2023-09-25 14:19:27 -0500

    Cambios en Boards

--- gestion_tareas/models/board.py
+++ gestion_tareas/models/board.py
@@ -5,4 +5,19 @@ class Board(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    options_month = [
+        ('ENE','Enero'),
+        ('FEB','Febrero'),
+        ('MAR','Marzo'),
+        ('ABR','Abril'),
+        ('MAY','Mayo'),
+        ('JUN','Junio'),
+        ('JUL','Julio'),
+        ('AGO','Agosto'),
+        ('SEP','Septiembre'),
+        ('OCT','Octubre'),
+        ('NOV','Noviembre'),
+        ('DIC','Diciembre'),
+    ]
+    month = models.CharField(max_length=20, choices=options_month, default='ENE')
 
